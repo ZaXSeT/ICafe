a9d1b0dca058344437608402c9b6c82ec0e2d4bc
ZaXSeT <zackyxie123@gmail.com>
2026-06-10 02:56:36 +0700

    chore: remove redundant vercel deploy workflow

--- flutter-app/lib/firebase_options.dart
+++ flutter-app/lib/firebase_options.dart
@@ -10,25 +10,56 @@ import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
+/// Default [FirebaseOptions] for use with your Firebase apps.
+///
+/// Example:
+/// ```dart
+/// import 'firebase_options.dart';
+/// // ...
+/// await Firebase.initializeApp(
+///   options: DefaultFirebaseOptions.currentPlatform,
+/// );
+/// ```
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return android;
       case TargetPlatform.iOS:
         return ios;
+      case TargetPlatform.macOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for windows - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
       default:
         throw UnsupportedError(
           'DefaultFirebaseOptions are not supported for this platform.',
         );
     }
   }
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyAl50PUxncpZd3FCKrxoSznr3bmQSBPtrk',
+    appId: '1:534641100622:web:e01ac1b3e3fafdfc31401f',
+    messagingSenderId: '534641100622',
+    projectId: 'icafe-add1f',
+    authDomain: 'icafe-add1f.firebaseapp.com',
+    storageBucket: 'icafe-add1f.firebasestorage.app',
+  );
 
   // TODO: Ganti nilai-nilai di bawah dengan config Firebase project kamu.
   // Ambil dari: Firebase Console > Project Settings > General > Your apps
